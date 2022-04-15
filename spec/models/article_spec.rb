b3e5ebbe221ded7e2e997fb8568d5f67878a9a45
princecoder51002 <wadhwacorporations51002@gmail.com>
2022-04-15 14:48:26 +0530

    Article model testing an article object with validations

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+
+  describe '#validations' do 
+
+    let(:article)  {FactoryBot.build(:article)}
+
+    it "tests the factory is valid" do 
+      # article = Article.create({title: 'Sample Article', content: 'Sample Content'})
+      expect(article).to(be_valid) # article.valid? == true
+    end
+  
+    it "has an invalid title" do 
+     article.title = ''
+     expect(article).not_to be_valid
+     expect(article.errors[:title]).to(include("can't be blank"))
+    end
+  
+    it "has an invalid content" do 
+     article.content = ''
+     expect(article).not_to be_valid
+     expect(article.errors[:content]).to include("can't be blank")
+    end
+  
+    it "has an invalid slug" do 
+      article.slug = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:slug]).to include("can't be blank")
+    end
+  end
+
+end
